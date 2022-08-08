bae203002cd5266fd7306320986a96e2735e2227
Mihaela Vlad <vlad.c.mihaela@gmail.com>
2022-08-08 21:57:57 +0300

    M01S06 ex01-04

--- M01S06/ex04/app.js
+++ M01S06/ex04/app.js
@@ -1,5 +1,22 @@
-(function() {
-  document.addEventListener('DOMContentLoaded', function() {
-      
+//  siaf - iife
+(function () {
+  document.addEventListener('DOMContentLoaded', function () {
+    const stage = document.querySelector('.stage');
+
+    stage.addEventListener('mouseover', function () {
+      console.log('Mouseul este pe scena');
+      const p = document.createElement('p');
+      p.innerText = 'Mouseul este pe scena';
+
+      document.body.append(p);
+    });
+
+    stage.addEventListener('mouseout', function () {
+      console.log('Mouseul nu este pe scena');
+      const p = document.createElement('p');
+      p.innerText = 'Mouseul nu este pe scena';
+
+      document.body.append(p);
+    });
   });
 })();
